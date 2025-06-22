10750b0cfcbd1baa23e66346c8fef4ef5c2f2e53
pdh0128 <pdh0128a@gmail.com>
2025-06-23 03:04:03 +0900

    fix :: test 수정

--- src/main/java/com/example/anime/domain/character/model/CharacterState.java
+++
@@ -1,5 +0,0 @@
-package com.example.anime.domain.character.model;
-
-public enum CharacterState {
-  NOT_MEMORIALIZING, MEMORIALIZING
-}
--- src/main/java/com/example/anime/global/storage/FileStorage.java
+++
@@ -1,9 +0,0 @@
-package com.example.anime.global.storage;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface FileStorage {
-  String upload(String characterId, MultipartFile file) throws IOException;
-}
