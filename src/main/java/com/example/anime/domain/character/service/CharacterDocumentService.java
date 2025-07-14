58bdb7c148ac3ebed0e520265f92d312f01c025c
comodo <comodo@comodos-MacBook.local>
2025-07-14 15:36:10 +0900

    refactor :: 공통 로직 분리

--- src/main/java/com/example/anime/domain/character/service/CharacterDocumentService.java
+++ src/main/java/com/example/anime/domain/character/service/CharacterDocumentService.java
@@ -1,5 +1,6 @@
 package com.example.anime.domain.character.service;
 
+import co.elastic.clients.util.TriFunction;
 import com.example.anime.domain.character.dto.response.CharacterResponse;
 import com.example.anime.domain.character.mapper.CharacterDocumentMapper;
 import com.example.anime.domain.character.model.CharacterDocument;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.BiFunction;
 
 @Service
 @RequiredArgsConstructor
@@ -18,19 +20,35 @@ public class CharacterDocumentService {
     private final CharacterDocumentMapper characterDocumentMapper;
 
     public CursorPage<CharacterResponse> findAllByName(String name, Long cursorId, int size) {
-        DocumentSlice<CharacterDocument> characterDocumentSlice = cursorId == null
-                ? characterDocumentRepository.findCharactersByName(size, name)
-                : characterDocumentRepository.findCharactersByCursorIdAndName(cursorId, size, name);
-        List<CharacterResponse> characterList = characterDocumentMapper.toCharacterList(characterDocumentSlice);
-        return new CursorPage<>(characterList, characterDocumentSlice.hasNext());
+        return findCharacter(
+                cursorId,
+                size,
+                name,
+                characterDocumentRepository::findCharactersByName,
+                characterDocumentRepository::findCharactersByCursorIdAndName
+        );
     }
 
-
     public CursorPage<CharacterResponse> findAllByDeathReason(String deathReason, Long cursorId, int size) {
-        DocumentSlice<CharacterDocument> characterDocumentSlice = cursorId == null
-                ? characterDocumentRepository.findCharactersByDeathReason(size, deathReason)
-                : characterDocumentRepository.findCharactersByCursorIdAndDeathReason(cursorId, size, deathReason);
+        return findCharacter(
+                cursorId,
+                size,
+                deathReason,
+                characterDocumentRepository::findCharactersByDeathReason,
+                characterDocumentRepository::findCharactersByCursorIdAndDeathReason
+        );
+    }
 
+    private CursorPage<CharacterResponse> findCharacter(
+            Long cursorId,
+            int size,
+            String feild,
+            BiFunction<Integer, String, DocumentSlice<CharacterDocument>> findCharacter,
+            TriFunction<Long, Integer, String, DocumentSlice<CharacterDocument>> findCharacterByCursorId
+    ) {
+        DocumentSlice<CharacterDocument> characterDocumentSlice = cursorId == null
+                ? findCharacter.apply(size, feild)
+                : findCharacterByCursorId.apply(cursorId, size, feild);
         List<CharacterResponse> characterList = characterDocumentMapper.toCharacterList(characterDocumentSlice);
         return new CursorPage<>(characterList, characterDocumentSlice.hasNext());
     }
